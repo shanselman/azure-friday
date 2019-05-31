211ac8b31324ded3dec40e1584585fb7b396b410
asincole <asincole@gmail.com>
2019-05-31 03:08:09 +0100

    add page handler

--- azure-friday.core/Pages/Index.cshtml.cs
+++ azure-friday.core/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using azure_friday.core.services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,21 @@ namespace azure_friday.core.Pages
 {
     public class IndexModel : PageModel
     {
+        private IAzureFridayDB _db;
+        public List<Video> Videos { get; set; }
+
+        public IndexModel(IAzureFridayDB db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+        }
 
+        public async Task<JsonResult> OnGetLoadVideos()
+        {
+            var videos = await _db.GetVideos();
+            return new JsonResult(videos);
         }
     }
 }
