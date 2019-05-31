94f88022f23c4177968fd87d10c80c9de3395c86
asincole <asincole@gmail.com>
2019-05-31 03:02:21 +0100

    add lazycache add db service add Nlog

--- azure-friday.core/Startup.cs
+++ azure-friday.core/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using azure_friday.core.services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Polly;
 
 namespace azure_friday.core
 {
@@ -33,11 +37,25 @@ namespace azure_friday.core
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register LazyCache
+            services.AddLazyCache();
+
+            services.AddHttpClient<AzureFridayClient>()
+                .AddTransientHttpErrorPolicy(PolicyBuilder => PolicyBuilder.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)))
+                .AddTransientHttpErrorPolicy(policyBuilder => policyBuilder.CircuitBreakerAsync(
+                    handledEventsAllowedBeforeBreaking: 2,
+                    durationOfBreak: TimeSpan.FromMinutes(1)
+                )
+            );
+
+            services.AddSingleton<IAzureFridayDB, AzureFridayDB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddNLog();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
