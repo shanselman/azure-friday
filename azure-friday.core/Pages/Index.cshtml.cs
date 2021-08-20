e3efabd16b3470e306f469e72b6cd50ddccb4c03
Scott Hanselman <scott@hanselman.com>
2021-08-20 16:45:16 -0700

    Azure friday url shortener!

--- azure-friday.core/Pages/Index.cshtml.cs
+++ azure-friday.core/Pages/Index.cshtml.cs
@@ -11,11 +11,23 @@ namespace azure_friday.core.Pages {
         private IAzureFridayDB _db;
         public List<Video> Videos { get; set; }
 
-        public IndexModel (IAzureFridayDB db) {
+        public IndexModel(IAzureFridayDB db) {
             _db = db;
         }
 
-        public void OnGet () { }
+        public IActionResult OnGet(int? id, string path) {
+
+            //did the url have an id?
+            // left pad with zeros 12 => 012 
+            // Redirect to https://aka.ms/azfr/{paddedId}
+            //else, continue 
+            if (id.HasValue)
+            {
+                var stringId = String.Format("{0:000}", id);
+                return Redirect($"https://aka.ms/azfr/{stringId}");
+            }
+            return Page();        
+        }
 
         public async Task<JsonResult> OnGetLoadVideos () {
             var videos = await _db.GetVideos ();
